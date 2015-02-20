399df5a36b1fdc4da48cb0f5d9dee4ba7848a648
Vincent KHERBACHE <vincent.kherbache@inria.fr>
2015-02-20 19:49:51 +0100

    added tests for micro-bench in nancy

--- src/test/java/ExecutorTest.java
+++ src/test/java/ExecutorTest.java
@@ -8,12 +8,24 @@ import java.io.IOException;
 public class ExecutorTest {
 
     @Test
-    public void test() throws IOException {
+    public void intraNodeTest() throws IOException {
 
         G5kExecutor.main(new String[]{
-                "--timeout", "500",
-                "-i", "src/test/resources/4n-4v.json",
-                "-o", "src/test/resources/"
+                "-s", "src/main/bin/scripts/",
+                "-i", "src/test/resources/micro_intra-node.json",
+                "-o", "src/test/resources/micro_intra-node.csv"
+        });
+        System.err.flush();
+        System.out.flush();
+    }
+
+    @Test
+    public void interNodeTest() throws IOException {
+
+        G5kExecutor.main(new String[]{
+                "-s", "src/main/bin/scripts/",
+                "-i", "src/test/resources/micro_inter-node.json",
+                "-o", "src/test/resources/micro_inter-node.csv"
         });
         System.err.flush();
         System.out.flush();
