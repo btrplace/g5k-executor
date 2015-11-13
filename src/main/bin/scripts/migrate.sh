63f552681021635b7cf6b459221f4a979776306a
Vincent KHERBACHE <vincent.kherbache@inria.fr>
2015-11-13 12:44:59 +0100

    remove useless img parts on migrate script

--- src/main/bin/scripts/migrate.sh
+++ src/main/bin/scripts/migrate.sh
@@ -14,11 +14,6 @@ NODE_DEST=`cat translate | grep -P "$NODE_DEST\t" | awk '{print $2;}'`
 SSH_USER="root"
 SSH_OPTS=' -o StrictHostKeyChecking=no -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o LogLevel=quiet '
 
-# Add custom vars
-VM_BASE_IMG='/home/vinkherbache/images/ubuntu_desktop_10G.img'
-VM_BASE_IMG_NAME=$(basename $VM_BASE_IMG)
-VM_BASE_IMG_DIR="/data/nfs/base_img"
-VM_BACKING_IMG_DIR="/data/nfs/backing"
 
 # Convert Mb/s to MiB/s and round
 BANDWIDTH_OCTET=`bc <<< "$BANDWIDTH/8.388608"`
@@ -26,32 +21,6 @@ BANDWIDTH_OCTET=`bc <<< "$BANDWIDTH/8.388608"`
 START=$(date +%s)
 echo -e "Start:\tMigrate $VM_NAME from $NODE_SRC to $NODE_DEST at $BANDWIDTH Mbps ($BANDWIDTH_OCTET MiB/s)"
 
-if [ -n "$VM_BACKING_IMG_DIR" ]; then
-        SRC_IMG="$VM_BASE_IMG_DIR/$VM_BASE_IMG_NAME"
-else
-        SRC_IMG="$VM_BASE_IMG_DIR/$VM_NAME.${VM_BASE_IMG##*.}"
-fi
-
-# Create base img / backing img to the destination node
-if [ -n "$VM_BACKING_IMG_DIR" ]; then
-        # If backing img is not on shared storage
-        if [ "$VM_BACKING_IMG_DIR" == "/tmp" ]; then
-                # Create a qcow2 backing img file if it doesn't already exist
-                BACKING_IMG="$VM_BACKING_IMG_DIR/$VM_NAME.qcow2"
-                if ( ssh $SSH_USER@$NODE_DEST $SSH_OPTS ''[ -e $BACKING_IMG ]'' ); then
-                        ssh $SSH_USER@$NODE_DEST $SSH_OPTS "rm -rf $BACKING_IMG 2>/dev/null"
-                fi
-                ssh $SSH_USER@$NODE_DEST $SSH_OPTS "qemu-img create -f qcow2 -o backing_file=$SRC_IMG,backing_fmt=raw $BACKING_IMG >/dev/null"
-
-        fi
-else
-        # If base img is not on shared storage
-        if [ "$VM_BASE_IMG_DIR" == "/tmp" ]; then
-                # Create a new empty img with the same size as the src img
-                IMG_SIZE=$(ssh $SSH_USER@$NODE_SRC $SSH_OPTS "du -b $SRC_IMG | cut -f1")
-                ssh $SSH_USER@$NODE_DEST $SSH_OPTS "dd if=/dev/zero of=$SRC_IMG bs=1 count=0 seek=$IMG_SIZE >/dev/null"
-        fi
-fi
 
 # Set bandwidth
 virsh --connect qemu+tcp://$NODE_SRC/system migrate-setspeed $VM_NAME --bandwidth $BANDWIDTH
@@ -63,18 +32,6 @@ virsh --connect qemu+tcp://$NODE_SRC/system migrate $VIRSH_OPTS $VM_NAME qemu+tc
 ssh $SSH_USER@$NODE_DEST $SSH_OPTS "sudo /etc/init.d/libvirt-bin start"
 ssh $SSH_USER@$NODE_SRC $SSH_OPTS "sudo /etc/init.d/libvirt-bin start"
 
-# Delete base img/backing img from source node
-if [ -n "$VM_BACKING_IMG_DIR" ]; then
-        # If backing img is not on shared storage
-        if [ "$VM_BACKING_IMG_DIR" == "/tmp" ]; then
-                ssh $SSH_USER@$NODE_SRC $SSH_OPTS "rm -rf $VM_BACKING_IMG_DIR/$VM_NAME.qcow2"
-        fi
-else
-        # If base img is not on shared storage
-        if [ "$VM_BASE_IMG_DIR" == "/tmp" ]; then
-                ssh $SSH_USER@$NODE_SRC $SSH_OPTS "rm -rf $SRC_IMG"
-        fi
-fi
 
 END=$(date +%s)
 echo -e "End:\tMigrate $VM_NAME from $NODE_SRC to $NODE_DEST at $BANDWIDTH Mbps ($BANDWIDTH_OCTET MiB/s)\t(time=$(($END - $START)))s"
